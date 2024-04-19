fe90ec8c33afcde8bf2602457a268830211ac9d9
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-04-19 16:59:02 +0700

    init project & login form

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+   <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .container {
+    width: 100%;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 15px;
+    padding-left: 15px;
+}
+
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+}
+
+.col-md-12 {
+    flex: 0 0 100%;
+    max-width: 100%;
+}
+
+</style>
